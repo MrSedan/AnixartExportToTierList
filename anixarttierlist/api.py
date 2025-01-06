29b55c2df4e113da36eb40d09033d0db188587bc
Sergey Elpashev <elpashevs@bk.ru>
2025-01-06 23:46:29 +0300

    Feat: user agent for downloading

--- anixarttierlist/api.py
+++ anixarttierlist/api.py
@@ -32,7 +32,9 @@ def get_image_url(name: str) -> str:
 
 def download_image(url: str, name: str, save_folder: str = './animes'):
     try:
-        img_data = requests.get(url).content
+        # User agent for right image save
+        img_data = requests.get(url, headers={
+                                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}).content
         with open(f"{save_folder}/{name}.jpeg", 'wb') as handler:
             handler.write(img_data)
     except Exception as e:
